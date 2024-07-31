54156eddb771802e525443fcac909d33853ca0d8
Archit Shetty <163320616+Archit-Shetty@users.noreply.github.com>
2024-07-31 17:14:50 +0530

    Update methodOverload2.java

--- Classes And Objects/methodOverload2.java
+++ Classes And Objects/methodOverload2.java
@@ -19,7 +19,7 @@ class shapeAreaPerimeter {
     }
 }
 
-class shape2d {
+class methodOverload2.java {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter length = ");
