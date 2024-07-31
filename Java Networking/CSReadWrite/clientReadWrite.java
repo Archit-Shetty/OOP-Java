fb85720aaafb8cbc727a8870ed11702931a64752
Archit Shetty <163320616+Archit-Shetty@users.noreply.github.com>
2024-07-31 18:15:09 +0530

    Create clientReadWrite.java

---
+++ Java Networking/CSReadWrite/clientReadWrite.java
@@ -0,0 +1,20 @@
+//Client sends "Hello server" to server and server sends "Hello client" then client reads and prints it on the console.
+//Client program
+import java.io.*;
+import java.net.*;
+
+class clientReadWrite {
+    public static void main(String[] args) {
+        try{
+            Socket s = new Socket("localhost", 5000);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            dout.writeUTF("Hello Server");
+            String msg = (String)din.readUTF();
+            System.out.println("The message from the server is: " + msg);
+            s.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }    
+}
