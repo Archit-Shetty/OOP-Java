161708dbedad6fa44d1d0f46dec6b152465d5377
Archit Shetty <163320616+Archit-Shetty@users.noreply.github.com>
2024-07-31 17:44:08 +0530

    Create welcome.java

---
+++ JSP and Servlets/welcome/welcome.java
@@ -0,0 +1,26 @@
+/* Create a dynamic web application to perform following operations:
+a) Create a html file to read first name and last name
+b) On clicking the submit button servlet prints welcome message along with the name on web browser */
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(description = "welcome user", urlPatterns = { "/welcome" })
+public class welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String first = request.getParameter("fn");
+		String last = request.getParameter("ln");
+		
+		out.println("<html><body>");
+		out.println("Welcome <b>" + first + " " + last + "</b>");
+		out.println("</html></body>");
+	}
+}
